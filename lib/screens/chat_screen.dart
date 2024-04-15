ac0b1c19533d3787c453f349e5144f8bba578447
Saad Idriss <saadidriss721@gmail.com>
2024-04-14 22:36:08 -0400

    fully functional back end update

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,115 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:messaging_app/components/chat_service.dart';
+import 'package:messaging_app/components/my_textfield.dart';
+
+class ChatPage extends StatefulWidget {
+  final String receiverUserEmail;
+  final String receiverUserID;
+  const ChatPage({
+    super.key,
+    required this.receiverUserEmail,
+    required this.receiverUserID,
+  });
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageController = TextEditingController();
+  final ChatServices _chatServices = ChatServices();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void sendMessage() async {
+    //only send message if there is an input
+    if (_messageController.text.isNotEmpty) {
+      await _chatServices.sendMessage(
+          widget.receiverUserID, _messageController.text);
+      //clear textbox after a message is sent
+      _messageController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.receiverUserEmail)),
+      body: Column(
+        children: [
+          //message
+          Expanded(
+            child: _buildMessageList(),
+          ),
+          //user input
+          _buildMessageInput(),
+        ],
+      ),
+    );
+  }
+
+  // build message list
+  Widget _buildMessageList() {
+    return StreamBuilder(
+        stream: _chatServices.getMessages(
+            widget.receiverUserID, _firebaseAuth.currentUser!.uid),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error${snapshot.error}');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading...');
+          }
+
+          return ListView(
+            children: snapshot.data!.docs
+                .map((document) => _buildMessageItem(document))
+                .toList(),
+          );
+        });
+  }
+
+  // build message item
+  Widget _buildMessageItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+    // align message to sender side and receiver side
+    var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
+
+    return Container(
+      alignment: alignment,
+      child: Column(
+        children: [
+          Text(data['senderEmail']),
+          Text(data['message']),
+        ],
+      ),
+    );
+  }
+
+  // build message input
+  Widget _buildMessageInput() {
+    return Row(
+      children: [
+        // textfield
+        Expanded(
+          child: MyTextField(
+            controller: _messageController,
+            hintText: 'Enter message',
+            obscureText: false,
+          ),
+        ),
+        // send button
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(Icons.arrow_right, size: 40),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:messaging_app/screens/sign_in_screen.dart';
 import 'package:messaging_app/auth/auth_page.dart';
+import 'package:messaging_app/screens/chat_screen.dart';
+
+// Define _auth as an instance of FirebaseAuth
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -13,6 +17,7 @@ class HomeScreen extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.logout),
             onPressed: () {
+              // sign user out
               FirebaseAuth.instance.signOut();
               Navigator.pushReplacement(
                 context,
@@ -22,15 +27,52 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          'Welcome to my App!',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+      body: _buildUserList(),
     );
+  }
+
+  // build a list of users except for the current log
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('error');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading...');
+        }
+        return ListView(
+            children: snapshot.data!.docs
+                .map<Widget>(
+                  (doc) => _buildUserListItem(context, doc),
+                )
+                .toList());
+      },
+    );
+  }
+
+  //build individual user list items
+  Widget _buildUserListItem(BuildContext context, DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    //display all users except current users
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: Text(data['email']),
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatPage(
+                  receiverUserEmail: data['email'],
+                  receiverUserID: data['uid'],
+                ),
+              ));
+        },
+      );
+    } else {
+      return Container();
+    }
   }
 }
